658b049ce2a6c0bd481c247040c4a46183d0ffd3
Félix Antonio Serna Olguín <fantonioserna18@gmail.com>
2021-06-21 12:09:51 -0500

    Update page.php

--- wordpress/wp-content/themes/my-theme/page.php
+++ wordpress/wp-content/themes/my-theme/page.php
@@ -7,9 +7,21 @@
         <article class="article-full">
             <header>
                 <h2><?php the_title(); ?></h2>
-                
             </header>
+            <?php the_content(); ?>
         </article>
 
+        <?php endwhile; else : ?>
+
+        <article>
+            <p>Lo sentimos, no hay publicaciones para mostrar.</p>
+        </article>
+        <?php endif; ?>
+
     </section>
+
+    <?php get_sidebar(); ?>
+
 </main>
+
+<?php get_footer(); ?>
