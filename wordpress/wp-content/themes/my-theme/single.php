5cbe6aefbc6fe2f439fd028c11cc6a8ef7ca4803
Félix Antonio Serna Olguín <fantonioserna18@gmail.com>
2021-06-21 10:48:15 -0500

    Commit

---
+++ wordpress/wp-content/themes/my-theme/single.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<main class="wrap">
+    <section class="content-area content-full-width">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <article class="article-full">
+            <header>
+                <h2>
+                    <?php the_title(); ?>
+                </h2>
+                By: <?php the_author(); ?>
+            </header>
+            
+            <?php the_content(); ?>
+        </article>
+
+        <?php endwhile; else : ?>
+
+        <article>
+            <p>Lo sentimos, no encontramos publicaciones para mostrar.</p>
+        </article>
+
+        <?php endif; ?>
+
+    </section>
+</main>
+
+<?php get_footer(); ?>
