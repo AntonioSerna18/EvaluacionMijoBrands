ac8b627842fd5410a3e098a2d939bec42527af20
Félix Antonio Serna Olguín <fantonioserna18@gmail.com>
2021-06-21 13:28:15 -0500

    Update functions.php

--- wordpress/wp-content/themes/my-theme/functions.php
+++ wordpress/wp-content/themes/my-theme/functions.php
@@ -3,21 +3,21 @@
         wp_enqueue_style( 'normalize-styles', "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" );
     }
 
-    function add_widget_Support() {
-        register_sidebar( array(
-            'name'  => 'Sidebar',
-            'id'    => 'sidebar',
-            'before_widget' => '<div>',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h2>',
-            'after_title'   => '</h2>',
-        ));
-    }
+        function add_widget_Support() {
+            register_sidebar( array(
+                'name'  => 'Sidebar',
+                'id'    => 'sidebar',
+                'before_widget' => '<div>',
+                'after_widget'  => '</div>',
+                'before_title'  => '<h2>',
+                'after_title'   => '</h2>',
+            ));
+        }
 
-    add_action( 'widgets_init', 'add_widget_Support');
+        add_action( 'widgets_init', 'add_widget_Support');
 
-    function add_Main_Nav() {
-        register_nav_menu('header-menu', _( 'Header Menu' ));
-    }
+        function add_Main_Nav() {
+            register_nav_menu('header-menu', _( 'Header Menu' ));
+        }
 
-    add_action( 'init', 'add_Main_Nav' );
+        add_action( 'init', 'add_Main_Nav' );
