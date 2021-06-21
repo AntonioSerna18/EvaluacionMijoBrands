f7df191d726aa8c8fb0d43ed59800406afb4ec09
Félix Antonio Serna Olguín <fantonioserna18@gmail.com>
2021-06-21 12:34:03 -0500

    Update index.php

--- wordpress/wp-content/themes/my-theme/index.php
+++ wordpress/wp-content/themes/my-theme/index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <main class="wrap">
+
     <section class="content-area content-thin">
         
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
